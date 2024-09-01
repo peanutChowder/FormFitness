67389fcefc0380bde02e25be5a8569fdc53fe845
Jacob Feng <peanutchowder@gmail.com>
2024-08-31 23:49:22 -0700

    Initial Commit

---
+++ FormFitness/FormFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormFitnessApp.swift
+//  FormFitness
+//
+//  Created by Jacob Feng on 8/31/24.
+//
+
+import SwiftUI
+
+@main
+struct FormFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
